8b4db1549809e7be53a78d99fb3c56b0b316b4c4
Orbit_Gamerz <gklibrary2020@gmail.com>
2023-09-30 00:47:53 +0530

    some error solved

--- Auth/Register.js
+++ Auth/Register.js
@@ -1,13 +1,13 @@
 import RegisterUsers from "../Schema/register.js";
 const registerUsers = async (req, res) => {
-  console.log(req.body);
-  const userdata = req.body;
-  try {
-    const createUser = await RegisterUsers.create(userdata);
-    res.json({ message: "User Created.", user: createUser });
-  } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+    console.log(req.body);
+    const userdata  = req.body;
+    try {
+        const createUser = await RegisterUsers.create(userdata);
+        res.json({ message: "User Created.", user: createUser });
+      } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).json({ message: "Internal server error" });
+      }
+    };
 export default registerUsers;
